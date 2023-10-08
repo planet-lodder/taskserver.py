dece99bfb7a714efe9093cf41dd120d992e1738b
JohnnyBeProgramming <johnathan.lodder@gmail.com>
2023-10-08 18:30:57 +0200

    Fix task list

--- taskserver/routes/list.py
+++ taskserver/routes/list.py
@@ -3,7 +3,7 @@ from taskserver.models.TaskfileConfig import taskfile_for
 
 
 @router.get('/list')
-@router.renders("list")
+@router.renders("list/index")
 def taskList(req, resp):
     taskfile = {"_path": task_server.location}
     return {
@@ -13,7 +13,7 @@ def taskList(req, resp):
 
 
 @router.post('/list')
-@router.renders("list")
+@router.renders("list/index")
 def taskSearch(req, resp):
     terms = "" if req.body and not "search" in req.body else req.body["search"]
     taskfile = {"_path": task_server.location}
