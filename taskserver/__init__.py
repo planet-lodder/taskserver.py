23c511a06cede67ff57b71065ea39b83a5b8a445
JohnnyBeProgramming <johnathan.lodder@gmail.com>
2023-10-08 17:37:00 +0200

    Move breakdoiwn into execution

--- taskserver/__init__.py
+++ taskserver/__init__.py
@@ -1,8 +1,8 @@
 
-from taskserver.routes.main import router, task_server
+from taskserver.routes.index import router, task_server
 
 import taskserver.routes.sidenav
-import taskserver.routes.breakdown
+import taskserver.routes.execution.breakdown
 import taskserver.routes.views
 import taskserver.routes.list
 import taskserver.routes.config
