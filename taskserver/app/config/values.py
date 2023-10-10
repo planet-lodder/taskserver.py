42b7b23588ab0a1aba0e2e47a36f9038774378d6
JohnnyBeProgramming <johnathan.lodder@gmail.com>
2023-10-10 19:47:10 +0200

    Add config values to use cases

--- taskserver/app/config/values.py
+++ taskserver/app/config/values.py
@@ -2,58 +2,13 @@ from taskserver import router
 from taskserver.domain.handlers.serializers import Serialize, WebSerializer
 from taskserver.domain.use_cases.base import UseCase
 from taskserver.domain.use_cases.config import TaskConfigUseCase
-from taskserver.models.TaskfileConfig import taskfile_for
 from taskserver.utils import HtmxRequest
 
 
-def taskfile_value_update(req, resp, dest):
-    htmx = HtmxRequest(req)
-    taskfile = taskfile_for(req)
+class ConfigValueInput(WebSerializer):
+    def parse(self): return None
 
-    # Resolve the key name from the known trigger or prompt
-    key = htmx.prompt or ""
-    if not key and htmx.triggerName:
-        key = htmx.triggerName[len(f'config.{dest}.'):]
-
-    value = "" if not key else htmx.input(f'config.{dest}.{key}') or ""
-
-    print(f'Update config.{dest}: {taskfile._path}')
-    print(f' --> config[{dest}][{key}] == {value}')
-    values = taskfile[dest] = taskfile[dest] if dest in taskfile else {}
-    if not key in values or not value == values[key]:
-        # Update the value that is stored in memory
-        values[key] = value
-
-    data = {
-        "dest": dest,
-        "key": key,
-        "value": value,
-        "focus": True,
-        "taskfile": taskfile,
-        "placeholder": "Enter new value here",
-    }
-
-    return data
-
-
-def taskfile_value_remove(req, resp, dest):
-    taskfile = taskfile_for(req)
-    htmx = HtmxRequest(req)
-    key = htmx.triggerName
-
-    if not key:
-        raise Exception("Expected 'hx-trigger-name' header.")
-    if dest in taskfile and key in taskfile[dest]:
-        del taskfile[dest][key]
-
-    return taskfile
-
-
-class ValueChangedInput(WebSerializer):
-    def parse(self):
-        return self.forTarget("env")
-
-    def forTarget(self, dest):
+    def forUpdate(self, dest):
         htmx = HtmxRequest(self.req)
 
         # Resolve key for value that changed
@@ -77,27 +32,29 @@ class ValueChangedInput(WebSerializer):
 @router.renders("partials/values/item")
 def taskEditConfigEnv(req, resp):
     view = UseCase.forWeb(req, TaskConfigUseCase)
-    input = Serialize.fromWeb(req, ValueChangedInput)
-    dest, key, value = input.forTarget("env")
-    return view.updateValue(dest, key, value)
+    input = Serialize.fromWeb(req, ConfigValueInput)
+    return view.updateValue(*input.forUpdate("env"))
 
 
 @router.delete('/config/env')
 def taskRemoveConfigEnv(req, resp):
     view = UseCase.forWeb(req, TaskConfigUseCase)
-    input = Serialize.fromWeb(req, ValueChangedInput)
-    dest, key = input.forDelete("env")
-    view.removeValue(dest, key)
+    input = Serialize.fromWeb(req, ConfigValueInput)
+    view.removeValue(*input.forDelete("env"))
     return ""  # empty result
 
 
 @router.post('/config/vars')
 @router.renders("partials/values/item")
 def taskAddConfigEnv(req, resp):
-    return taskfile_value_update(req, resp, "vars")
+    view = UseCase.forWeb(req, TaskConfigUseCase)
+    input = Serialize.fromWeb(req, ConfigValueInput)
+    return view.updateValue(*input.forTarget("vars"))
 
 
 @router.delete('/config/vars')
 def taskRemoveConfigVars(req, resp):
-    taskfile_value_remove(req, resp, "vars")
-    return ""
+    view = UseCase.forWeb(req, TaskConfigUseCase)
+    input = Serialize.fromWeb(req, ConfigValueInput)
+    view.removeValue(*input.forDelete("vars"))
+    return ""  # empty result
