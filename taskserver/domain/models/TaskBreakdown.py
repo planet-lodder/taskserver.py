ccaf7d0eb20777daaa7f6dafa209086119cb3761
JohnnyBeProgramming <johnathan.lodder@gmail.com>
2023-11-02 17:12:06 +0100

    Tweak the task breakdowns

--- taskserver/domain/models/TaskBreakdown.py
+++ taskserver/domain/models/TaskBreakdown.py
@@ -32,18 +32,18 @@ class TaskCommand(Command):
 class TaskBreakdown(TaskCommand):
 
     @staticmethod
-    def forTask(filename, task_name):
+    def forTask(filename, task_name, force=False, up_to_date=False, existing_root=None, stack: List[TaskCommand] = []):
         # Do a dry run and capture the traced commands (to rebuild a execution tree)
-        command = f"{task_name} --dry --verbose -f"
+        command = f"{task_name} --dry --verbose"
+        command = command + ' -f' if force else command
         output, stderr, res = Taskfile.run(filename, command)
         if res.returncode > 0:
             message = f'Task "{task_name}" failed to run in dry run mode.'
             raise Exception(message)
 
         # Read the trace logs to reconstruct the task breakdown
-        root = TaskBreakdown(raw=task_name)
-        stack: List[TaskCommand] = []
-
+        root = existing_root or TaskBreakdown(
+            raw=task_name, up_to_date=up_to_date)
         debug = False
 
         def trace(message, prefix='', extra=''):
@@ -61,6 +61,7 @@ class TaskBreakdown(TaskCommand):
 
         buffer = stderr
         while match := re.search(r'((task: )(.*))+', buffer, re.MULTILINE):
+
             # Fetch the relevant parts from the regex match
             groups = match.groups()
             overflow = buffer[:match.start()].rstrip()
@@ -77,13 +78,16 @@ class TaskBreakdown(TaskCommand):
             if check := re.search(r'\"(.*)\" started', line):
                 # Task start tag detected
                 cmd_name = check.groups()[0]
-                trace(cmd_name, f' -> Task: ', 'started')
-                if not parent and cmd_name == task_name:
+                if existing_root and existing_root.raw == cmd_name:
+                    stack.append(root)
+                elif not parent and cmd_name == task_name:
                     # Root task is starting...
+                    trace(cmd_name, f' -> Task: ', 'root')
                     stack.append(root)
                 else:
                     # New sub task has started, add to parent
-                    cmd = TaskCommand(raw=cmd_name)
+                    trace(cmd_name, f' -> Task: ', 'started')
+                    cmd = TaskCommand(raw=cmd_name, up_to_date=up_to_date)
                     if parent:
                         parent.cmds.append(cmd)
                     stack.append(cmd)
@@ -91,7 +95,10 @@ class TaskBreakdown(TaskCommand):
                 # Task close tag has been detected
                 last = stack.pop()
                 cmd_name = check.groups()[0]
-                trace(cmd_name, f' <- Task: ', 'finished')
+                
+                if not existing_root or existing_root.raw != cmd_name:
+                    trace(cmd_name, f' <- Task: ', 'finished')
+                
                 if cmd_name == task_name and not len(stack) and (last != root or len(stack)):
                     # Main task has finished
                     warn(f'Commands are left on the stack after main task exits.')
@@ -103,7 +110,19 @@ class TaskBreakdown(TaskCommand):
                 last = stack.pop()
                 last.up_to_date = True
                 cmd_name = check.groups()[0]
+
+                # Forcefully get the sub task breakdown
+                full = TaskBreakdown.forTask(
+                    filename,
+                    cmd_name,
+                    True,
+                    up_to_date=up_to_date,
+                    existing_root=last,
+                    stack=stack,                    
+                )
+                # last.cmds = full.cmds
                 trace(cmd_name, f' <- Task: ', 'up to date')
+
                 if last.raw != cmd_name:
                     warn(f'Expected "task {cmd_name}", got "{last.text}".')
             elif check := re.search(r'\[(.*)\] (.*)', line):
@@ -112,7 +131,7 @@ class TaskBreakdown(TaskCommand):
                 cmd_raw = check.groups()[1]
                 if parent.raw == cmd_name:
                     trace('', f' -> Cmd: ' + cmd_raw)
-                    cmd = Command(raw=cmd_raw)
+                    cmd = Command(raw=cmd_raw, up_to_date=up_to_date)
                     parent.cmds.append(cmd)
                 else:
                     warn(f'Found an orphan command: {cmd_raw} ({cmd_name})')
@@ -123,4 +142,4 @@ class TaskBreakdown(TaskCommand):
         if buffer:
             parent.cmds[-1].raw = parent.cmds[-1].raw + buffer
 
-        return root.cmds
+        return root
