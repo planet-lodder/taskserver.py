4f58599000f97ef7630a30e31055f60470726b9b
JohnnyBeProgramming <johnathan.lodder@gmail.com>
2023-11-14 11:18:26 +0100

    Release 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import subprocess
 from distutils.core import setup
 
 name = 'taskserver'
-version = '0.0.2'
+version = '0.0.3'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
